52b04a2a818b4c38542169e738f33f7b82c8f233
Henry <33919933+AnonymousSFZ@users.noreply.github.com>
2019-06-04 10:07:27 +0800

    Update test_analysis.py

--- test_analysis.py
+++ test_analysis.py
@@ -6,23 +6,22 @@ ALGS = ["WAIT_DIE", "NO_WAIT", "DL_DETECT", "TIMESTAMP", "MVCC","HEKATON", "HSTO
 
 # headers
 for alg in ALGS:
-    print(alg[0:7] if len(alg) > 7 else alg, end='\t')
+    print("{: ^8s} ".format(alg[0:8] if len(alg) > 8 else alg), end='')
 print('BEST')
 # calculate throughput
-for i in range(1,9):
+for thd_cnt in range(1,9):
     max_throughput = 0
     max_alg = ''
     for alg in ALGS:
-        sim_time = 1
+        run_time = 1
         txn_cnt = 0
-        file = open(DIR + alg + "_t" + str(i) + ".txt").readlines()
+        file = open(DIR + alg + "_t" + str(thd_cnt) + ".txt").readlines()
         for line in file:
-            if 'SimTime' in line:
-                sim_time = int(line.split("= ")[-1])
             if '[summary]' in line:
                 txn_cnt = int(re.findall('txn_cnt=\d+', line)[0].split('=')[-1])
-        throughput = 10000 * txn_cnt / sim_time
-        print("%.5f " % throughput, sep='\t', end='')
+                run_time = float(re.findall('run_time=\d+.\d+', line)[0].split('=')[-1])
+        throughput = txn_cnt / run_time * thd_cnt
+        print("%8.1f " % throughput, sep='\t', end='')
         # get the best algorithm
         if max_throughput < throughput:
             max_throughput = throughput
